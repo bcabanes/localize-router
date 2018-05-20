dd23cc422ac77ea302cc232b9861889c8fd97431
bcabanes <contact@benjamincabanes.com>
2018-05-19 20:07:00 -0400

    test(project): all test passing

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -9,7 +9,7 @@
  */
 Error.stackTraceLimit = Infinity;
 
-require('core-js/es6');
+require('core-js/es6/reflect');
 require('core-js/es7/reflect');
 
 // Typescript emit helpers polyfill
@@ -17,11 +17,11 @@ require('ts-helpers');
 
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/proxy'); // since zone.js 0.6.15
+require('zone.js/dist/sync-test');
+require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/proxy'); // since zone.js 0.6.15
-require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
 
 // RxJS
 require('rxjs/Rx');
